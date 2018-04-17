430b4e6fd17251d3b794e6505c726e78163d1a4d
shantanu tripathi <shantanu.akt@gmail.com>
2018-04-17 19:11:16 +0530

    File Upload

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
 
 //MongoDB driver
 const mongoose = require('mongoose');
@@ -65,6 +66,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
 app.use('/leaders',leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
