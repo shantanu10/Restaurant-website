b31b92e8fb76d516657eb98e62e3b2d1880d37fd
shantanu tripathi <shantanu.akt@gmail.com>
2018-04-26 15:47:11 +0530

    Changed for deployment

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'mongoUrl' : process.env.MONGODB_URI,
     'facebook': {
         clientId: '187491685220802',
         clientSecret: '8e70fa66983e5e2d5867fcea162444c5'
